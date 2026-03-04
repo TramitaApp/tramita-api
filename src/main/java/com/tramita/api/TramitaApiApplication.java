cc574c69197caa19218865f7c90c01dceeb87614
retke@weg.net <retke@weg.net>
2026-03-04 20:15:19 -0300

    first commit

---
+++ src/main/java/com/tramita/api/TramitaApiApplication.java
@@ -0,0 +1,13 @@
+package com.tramita.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramitaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TramitaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tramita/api/TramitaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tramita.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TramitaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
